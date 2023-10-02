1072cda9b0ec9c1c74dbc1ac026d0fc94e89a71d
LeeJunO <126752196+junodevv@users.noreply.github.com>
2023-10-03 07:29:52 +0900

    [unrated] Title: 접미사 배열, Time: 0.21 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public ArrayList<String> solution(String my_string) {
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(int i = 0; i < my_string.length(); i++){
+            answer.add(my_string.substring(i));
+            answer.sort(null);
+        }
+        
+        return answer;
+    }
+}
