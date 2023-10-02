5c13e9f1dfeb192b2b9d40cc0b37a3409bb4b450
LeeJunO <126752196+junodevv@users.noreply.github.com>
2023-10-03 05:25:35 +0900

    [unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/README.md
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 부분 문자열 이어 붙여 문자열 만들기 - 181911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181911?language=java) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 2.06 ms
+메모리: 72.4 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String[] my_strings, int[][] parts) {
-        String answer = "";
-        
-        for(int i = 0; i < parts.length; i++){
-            answer += my_strings[i].substring(parts[i][0], parts[i][1]+1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < my_strings.length; i++) {
+            sb.append(my_strings[i].substring(parts[i][0], parts[i][1] + 1));
         }
-        
-        return answer;
+
+        return sb.toString();
     }
 }
