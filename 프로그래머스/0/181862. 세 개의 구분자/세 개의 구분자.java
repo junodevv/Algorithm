4ee2f13fa1d22af04363fade16670510210349c5
LeeJunO <zhqkfxm0127@naver.com>
2024-02-19 11:05:54 +0900

    [level 0] Title: 세 개의 구분자, Time: 53.62 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String myStr) {
+        String[] answer = {};
+        
+        String[] splited = myStr.split("([abc]+)");
+        answer = Arrays.stream(splited)
+                .filter(i -> !i.equals(""))
+                .toArray(String[]::new);
+        if(answer.length<1){
+            answer = new String[] {"EMPTY"};
+        }
+        return answer;
+    }
+}
