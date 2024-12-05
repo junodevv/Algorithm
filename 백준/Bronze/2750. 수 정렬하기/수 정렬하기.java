7a26a780a337079e8733a19ea7dae6df52fde2e7
LeeJunO <zhqkfxm0127@naver.com>
2024-12-05 11:57:16 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -3,12 +3,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main{
-    private static int[] inputs;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder result = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        inputs = new int[N];
+        int[] inputs = new int[N];
         for(int i=0; i<N; i++){
             inputs[i] = Integer.parseInt(br.readLine());
         }
@@ -16,7 +15,9 @@ public class Main{
         for(int i=0; i<N-1; i++){
             for(int j=0; j<N-1-i; j++){
                 if(inputs[j]>inputs[j+1]){
-                    swap(j, j+1);
+                    int temp = inputs[j];
+                    inputs[j] = inputs[j+1];
+                    inputs[j+1] = temp;
                 }
             }
         }
@@ -25,11 +26,5 @@ public class Main{
             result.append(inputs[i]).append("\n");
         }
         System.out.print(result);
-    }
-    
-    private static void swap(int pre, int post){
-        int temp = inputs[pre];
-        inputs[pre] = inputs[post];
-        inputs[post] = temp;
     }
 }
